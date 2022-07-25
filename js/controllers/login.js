e85a037758ca4f0aaa4bd8b948b8c75b62cd7a36
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-07-24 17:02:00 -0700

    Auth prueba

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -5,6 +5,12 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
           google.accounts.id.initialize({
             client_id: '285606346890-n4n0u10t970qi4gjaegqepfjl444ei96.apps.googleusercontent.com',
             callback: function (params) {
+              $http.post(`${URL}/oauth/google`, { value: params }).then(function successCallback(response) {
+                console.log(response)
+                sessionStorage.setItem('oauth_token', response);
+              }, function errorCallback(response) {
+              }
+              );
               console.log(params);
             }
           });
