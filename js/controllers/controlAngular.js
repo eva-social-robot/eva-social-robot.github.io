2c32b055ff7eb2cf38c4c29c38e0a92fe8cb2390
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-07-24 17:46:53 -0700

    Autenticacion

--- js/controllers/controlAngular.js
+++ js/controllers/controlAngular.js
@@ -1,2 +1,9 @@
-eva.controller("ControlEvaController", function ($scope, $http, $sce) {
+eva.controller("ControlEvaController", function ($scope, $http) {
+
+    $scope.status = function () {
+        $http.get(`${URL}/status`).then(function successCallback(response) {
+        }, function errorCallback(response) {
+        });
+    }
+    $scope.status();
 });
--- js/controllers/login.js
+++ js/controllers/login.js
@@ -5,9 +5,10 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
           google.accounts.id.initialize({
             client_id: '285606346890-n4n0u10t970qi4gjaegqepfjl444ei96.apps.googleusercontent.com',
             callback: function (params) {
-              $http.post(`${URL}/oauth/google`, { value: params }).then(function successCallback(response) {
-                console.log(response)
-                sessionStorage.setItem('oauth_token', response);
+              $http.post(`${URL}/oauth/google`, { value: params.credential }).then(function successCallback(response) {
+                console.log(response.credential);
+                sessionStorage.setItem('oauth_token', `Bearer ${response}`);
+                $http.defaults.headers.common['Authorization'] = sessionStorage.getItem('oauth_token')
               }, function errorCallback(response) {
               }
               );
