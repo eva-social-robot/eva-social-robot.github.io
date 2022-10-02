ec09aa6fa69fdcad5d2882c45906aea1f6bb39af
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-10-02 15:16:09 -0700

    Prueba

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -2,7 +2,7 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
 
     $scope.authenticate = function() {
         if (!sessionStorage.getItem('oauth_token')) {
-          windows.google.accounts.id.initialize({
+          window.google.accounts.id.initialize({
             client_id: '285606346890-n4n0u10t970qi4gjaegqepfjl444ei96.apps.googleusercontent.com',
             callback: function (params) {
               console.log(params);
