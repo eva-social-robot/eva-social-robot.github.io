2d24161878fb55d52ad220317ea2a738bcffd19b
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-10-02 14:58:22 -0700

    Prueba

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -5,6 +5,7 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
           google.accounts.id.initialize({
             client_id: '285606346890-n4n0u10t970qi4gjaegqepfjl444ei96.apps.googleusercontent.com',
             callback: function (params) {
+              console.log(params);
               $http.post(`${URL}/oauth/google`, { value: params.credential }).then(function successCallback(response) {
                 sessionStorage.setItem('oauth_token', `Bearer ${response.data.value}`);
                 $http.defaults.headers.common['Authorization'] = sessionStorage.getItem('oauth_token')
