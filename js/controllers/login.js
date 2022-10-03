339a1ea296bcec680dc1f56c1a523712916dbbfe
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-10-03 00:04:42 -0700

    Autenticacion local

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,21 +1,15 @@
 eva.controller("login", ['$scope', '$http', function ($scope, $http) {
 
     $scope.authenticate = function() {
-        if (!sessionStorage.getItem('oauth_token')) {
-          window.google.accounts.id.initialize({
-            client_id: '285606346890-n4n0u10t970qi4gjaegqepfjl444ei96.apps.googleusercontent.com',
-            callback: function (params) {
-              console.log(params);
-              $http.post(`${URL}/oauth/google`, { value: params.credential }).then(function successCallback(response) {
+      if (!sessionStorage.getItem('oauth_token')) {
+          alert("Please enter your email address");
+          $http.post(`${URL}/login`, { username: $scope.username, password: $scope.password }).then(function successCallback(response) {
                 sessionStorage.setItem('oauth_token', `Bearer ${response.data.value}`);
                 $http.defaults.headers.common['Authorization'] = sessionStorage.getItem('oauth_token')
               }, function errorCallback(response) {
-                console.log(response);
-              }
-              );
             }
-          });
-          google.accounts.id.prompt();
+          );
         }
     };
+
 }]);
