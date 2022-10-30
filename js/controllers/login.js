89e5b58d1cebae98544e042e57cd5c15985fdadf
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2022-10-30 15:57:50 -0700

    Cambios en el login

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -2,7 +2,9 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
 
     $scope.authenticate = function() {
       if (!sessionStorage.getItem('oauth_token')) {
-          alert("Please enter your email address");
+        if (!$scope.username || !$scope.password) {
+          alert("Please enter your email address or password");
+        } else {
           $http.post(`${URL}/login`, { username: $scope.username, password: $scope.password }).then(function successCallback(response) {
                 sessionStorage.setItem('oauth_token', `Bearer ${response.data.value}`);
                 $http.defaults.headers.common['Authorization'] = sessionStorage.getItem('oauth_token')
@@ -10,6 +12,6 @@ eva.controller("login", ['$scope', '$http', function ($scope, $http) {
             }
           );
         }
+      }
     };
-
 }]);
