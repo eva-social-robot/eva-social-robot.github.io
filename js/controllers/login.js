ba5d5ddea100b4e3c1f7a7e5688947db5011f7a8
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2023-01-22 23:09:52 -0800

    feat(web): :sparkles: Redirect if redirect attr in location

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -8,9 +8,16 @@ eva.controller("login", ['$scope', '$rootScope', 'AuthenticationService', '$loca
     $scope.authenticate = function() {
       AuthenticationService.Login($scope.username, $scope.password, function (result) {
         if (result === true) {
+          const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop),
+          });
+          if (params.redirect) {
+            window.location.replace(params.redirect);
+          } else {
             $location.path('/controlAngular');
             $rootScope.loggedIn = true;
             $rootScope.user = JSON.parse(atob(JSON.parse(sessionStorage.getItem('currentUser')).token.split('.')[1])).name || $scope.username;
+          }
         } else {
             // vm.error = 'Username or password is incorrect';
             $rootScope.loggedIn = false;
